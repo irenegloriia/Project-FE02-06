080a47a5adb38e1dbf417317ac660b4419594a78
Farhunson <45745245+Farhunson@users.noreply.github.com>
2020-12-18 17:28:02 +0800

    classdata grid

--- src/components/ClassesData/index.js
+++ src/components/ClassesData/index.js
@@ -1,11 +1,32 @@
 import React from 'react';
+import Grid from '@material-ui/core/Grid';
+import { makeStyles } from '@material-ui/core/styles';
+import Card from './Card';
+
+const useStyles = makeStyles({
+  GridContainer: {
+    paddingLeft: '20px',
+    paddingRight: '20px'
+  }
+});
 
 const ClassesData = () => {
-  return (
-    <div>
-      <h1>Class</h1>
-      {/* <ClassesData /> */}
-    </div>
+    const classes = useStyles();
+    return (
+      <div>
+        <h1>Class</h1>
+        <Grid container spacing={4} className={classes.GridContainer} justify="center">
+          <Grid item xs={12} sm={6} md={4}>
+            <Card />
+          </Grid>
+          <Grid item xs={12} sm={6} md={4}>
+            <Card />
+          </Grid>
+          <Grid item xs={12} sm={6} md={4}>
+            <Card />
+          </Grid>
+        </Grid>
+      </div>
   );
 };
 
