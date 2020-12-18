79d9f2b5c7fedc421693294ad574b8c02c961901
Irene Gloria <62594669+irenegloriia@users.noreply.github.com>
2020-12-18 22:28:22 +0700

    add teacherLesson

--- src/components/card/index.js
+++ src/components/card/index.js
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
+import Grid from '@material-ui/core/Grid';
 import CardActionArea from '@material-ui/core/CardActionArea';
 // import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
@@ -8,6 +9,7 @@ import CardContent from '@material-ui/core/CardContent';
 // import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
 import { CardMedia } from '@material-ui/core';
+import axios from 'axios';
 import { image2 } from '../../assets';
 import './style.css';
 
@@ -22,24 +24,64 @@ const useStyles = makeStyles({
 
 export default function MediaCard() {
   const classes = useStyles();
+  const [posts, setPosts] = useState([]);
+  useEffect(() => {
+    // setUserDataLoading(true);
+    axios
+      .get('http://54.242.150.44:3000/lessons', {
+        // eslint-disable-next-line
+        headers: {
+          Authorization:
+            'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjVmZGI4NDc3ZjE2YzBmNDViMDBmZjI1YiIsImlhdCI6MTYwODIyMTg2NSwiZXhwIjoxNjA4MzA4MjY1fQ.FVUGNDQrDEWDlyaHMkm_PRfXzr6x2WAtLedW_jYDBAE',
+        },
+      })
+      .then((res) => {
+        // eslint-disable-next-line
+        console.log(res.data.data);
+        setPosts(res.data.data);
+      })
+      .catch((error) => {
+        // eslint-disable-next-line
+        console.log(error);
+      })
+      .finally(() => {
+        // setUserDataLoading(false);
+      });
+  });
 
   return (
-    <Card className={classes.root}>
-      <CardActionArea>
-        <CardMedia>
-          <img alt="IM" className="picture" src={image2} />
-        </CardMedia>
-        <CardContent>
-          <Typography gutterBottom variant="h5" component="h2">
-            React Js
-          </Typography>
-          <Typography variant="body2" color="textSecondary" component="p">
-            Lizards are a widespread group of squamate reptiles
-          </Typography>
-        </CardContent>
-      </CardActionArea>
-      {/* <CardActions>
-      </CardActions> */}
-    </Card>
+    <Grid
+      container
+      direction="row"
+      justify="space-between"
+      alignItems="baseline"
+      spacing={1}
+    >
+      <Grid container item xs={12} spacing={3}>
+        {posts.map((res) => {
+          return (
+            <Card className={classes.root}>
+              <CardActionArea>
+                <CardMedia>
+                  <img alt="IM" className="picture" src={image2} />
+                </CardMedia>
+                <CardContent>
+                  <Typography gutterBottom variant="h5" component="h2">
+                    {res.name}
+                  </Typography>
+                  <Typography
+                    variant="body2"
+                    color="textSecondary"
+                    component="p"
+                  >
+                    {res.description}
+                  </Typography>
+                </CardContent>
+              </CardActionArea>
+            </Card>
+          );
+        })}
+      </Grid>
+    </Grid>
   );
 }
