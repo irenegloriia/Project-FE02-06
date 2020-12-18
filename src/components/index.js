2bb2181454291e5646ea51dd7cbe726c4e842f6f
Farhunson <45745245+Farhunson@users.noreply.github.com>
2020-12-18 12:29:36 +0800

    classespage

---
+++ src/components/index.js
@@ -0,0 +1,1 @@
+export { default as ClassesData } from './ClassesData'
--- src/pages/classes/index.js
+++ src/pages/classes/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
-
+import {ClassesData} from '../../components'
 const Classespage = () => {
   return (
     <div>
       <h1>Class</h1>
+      <ClassesData />
     </div>
   );
 };
