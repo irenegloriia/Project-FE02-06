d6cab0d99e3632711ab41214cabf51c434877e0f
Farhunson <45745245+Farhunson@users.noreply.github.com>
2020-12-18 17:33:58 +0800

    classes data

--- src/components/ClassesData/Card.js
+++ src/components/ClassesData/Card.js
@@ -4,7 +4,9 @@ import Card from '@material-ui/core/Card';
 import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
 import Button from '@material-ui/core/Button';
+import CardHeader from '@material-ui/core/CardHeader';
 import Typography from '@material-ui/core/Typography';
+
 
 const useStyles = makeStyles({
   root: {
@@ -29,6 +31,14 @@ export default function OutlinedCard() {
 
   return (
     <Card className={classes.root} variant="outlined">
+      <CardHeader 
+        action={
+            <IconButton aria-label="settings">
+              <MoreVertIcon />
+            </IconButton>
+        } 
+        title="Class title"
+      />
       <CardContent>
         <Typography className={classes.title} color="textSecondary" gutterBottom>
           Word of the Day
